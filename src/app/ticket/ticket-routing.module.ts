6e273c9c6a148dab41ccab4de0ee8e4efea8dd41
pankowskitomasz <pankowski.email@gmail.com>
2022-02-28 18:47:10 +0200

    routing

--- src/app/ticket/ticket-routing.module.ts
+++ src/app/ticket/ticket-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ComingsoonComponent } from './comingsoon/comingsoon.component';
+import { TicketComponent } from './ticket/ticket.component';
 
 const routes: Routes = [
   {
-    path:"comingsoon",
-    component: ComingsoonComponent
+    path:"ticket",
+    component: TicketComponent
   }
 ];
 
@@ -13,4 +13,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class ComingsoonRoutingModule { }
+export class TicketRoutingModule { }
